69780c7b92129388c66c008ff323d5ae6a0bfff8
junkesgui <guilherme.junkes1@gmail.com>
2025-10-28 04:03:23 -0300

    first commit

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.get('/health', (request, response) =>{
+  return response.json({message: 'Hello, Im healthy'});
+});
+
+export default routes;
---
+++ src/shared/http/server.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/index';
+require("express-async-errors");
+require('reflect-metadata');
+import ErrorHandlerMiddleware from '../middlewares/ErrorHandlerMiddleware';
+import { AppDataSource } from '../typeorm/data-source';
+
+AppDataSource.initialize().then(async() =>{
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+app.use(ErrorHandlerMiddleware.handleError)
+
+console.log('Connected to database')
+
+app.listen(3333, () =>{
+  console.log('Listening at port 3333')
+});
+
+
+}).catch(error =>{
+  console.error('Failed to connect to database: ', error)
+})
+
