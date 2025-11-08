97b53005260fbf36023c96dab090a6a74820949d
junkesgui <guilherme.junkes1@gmail.com>
2025-11-08 04:00:53 -0300

    fix: conserta a ligacao com a tabela products

--- src/shared/typeorm/data-source.ts
+++ src/shared/typeorm/data-source.ts
@@ -2,7 +2,7 @@ require('dotenv/config')
 require('reflect-metadata')
 import { DataSource } from "node_modules/typeorm/index";
 
-const port = process.env.PORT as number | undefined;
+const port = process.env.DB_PORT as number | undefined;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -11,6 +11,6 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: ['./src/modules/**/databse/entities/*.{ts,js}'],
-  migrations: ['./src/shared/typeorm/migrations/*.{ts,js}']
+  entities: ['./src/modules/**/database/entities/*.ts'],
+  migrations: ['./src/shared/typeorm/migrations/*.ts']
 })
