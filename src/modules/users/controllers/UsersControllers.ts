774b915438a0a1239c88a0575645b370f9dfd44c
junkesgui <guilherme.junkes1@gmail.com>
2025-11-10 03:52:59 -0300

    feat: cria usersController

---
+++ src/modules/users/controllers/UsersControllers.ts
@@ -0,0 +1,24 @@
+import type { Request, Response } from "express";
+import ListUsersService from "../services/ListUsersService";
+import CreateUserService from "../services/CreateUserService";
+
+export default class UsersControllers{
+  async index (request: Request, response: Response): Promise<Response>{
+    const listUsersService = new ListUsersService();
+    const users = await listUsersService.execute();
+    return response.json(users);
+  }
+
+  async create(request: Request, response: Response): Promise<Response>{
+    const { name, email, password } = request.body;
+    const createUserService = new CreateUserService();
+    const user = await createUserService.execute({
+      name,
+      email,
+      password
+    });
+    return response.json(user)
+  }
+}
+
+
