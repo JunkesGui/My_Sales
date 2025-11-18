fbd2b9b39be4d84183e28f954e6bb2a72c0eb0c2
junkesgui <guilherme.junkes1@gmail.com>
2025-11-18 04:24:31 -0300

    feat: cria os servicos dos customers

---
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -0,0 +1,24 @@
+import AppError from "@shared/errors/AppError";
+import { Customer } from "../database/entities/Customer";
+import { customerRepositories } from "../database/repositories/CustomerRepositories";
+
+interface ICreateCustomer{
+  name: string,
+  email: string
+}
+
+export default class CreateCustomerService{
+  public async execute({ name, email }: ICreateCustomer): Promise<Customer>{
+    const emailExists = await customerRepositories.findByEmail(email)
+
+    if (emailExists){
+      throw new AppError('Email already been taken', 409)
+    }
+
+    const customer = customerRepositories.create({ name, email })
+
+    await customerRepositories.save(customer)
+
+    return customer
+  }
+}
