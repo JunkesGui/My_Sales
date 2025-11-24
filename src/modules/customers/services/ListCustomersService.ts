1aa7e38f0405ceb4fec09f8a2cecc81820a250af
junkesgui <guilherme.junkes1@gmail.com>
2025-11-24 03:42:17 -0300

    refactor: modifica o service e controller do costumer para incluir paginacao

--- src/modules/customers/services/ListCustomersService.ts
+++ src/modules/customers/services/ListCustomersService.ts
@@ -1,10 +1,24 @@
-import { Customer } from "../database/entities/Customer";
+import { IPagination } from "@shared/interfaces/pagination.interface";
 import { customerRepositories } from "../database/repositories/CustomerRepositories";
+import { Customer } from "../database/entities/Customer";
 
 export default class ListCustomerService{
-  async execute(): Promise<Customer[]> {
-    const customers = await customerRepositories.find()
+  async execute(page: number = 1, limit: number = 10): Promise<IPagination<Customer>> {
+    const [data, total] = await customerRepositories.findAndCount({
+      take: limit,
+      skip: (page -1) * limit
+    })
 
-    return customers
+    const totalPages = Math.ceil(total / limit)
+
+    return {
+      per_page: limit,
+      total,
+      data,
+      current_page: page,
+      total_pages: totalPages,
+      next_page: page < totalPages ? page +1: null,
+      previous_page: page > 1 ? page -1: null,
+    }
   }
 }
