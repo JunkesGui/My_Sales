d7863775267f6d8728c60846ee58a532e7c017a6
junkesgui <guilherme.junkes1@gmail.com>
2026-01-23 04:47:04 -0300

    feat: cria o arquivo `DummyOrderRepositories`

---
+++ src/modules/orders/domain/repositories/dummies/DummyOrderRepositories.ts
@@ -0,0 +1,55 @@
+import { Order } from "@modules/orders/infra/database/entities/Order";
+import AppError from "@shared/errors/AppError";
+import { SearchParams } from "@shared/interfaces/SearchParams";
+import { ICreateOrder } from "../../models/ICreateOrder";
+import { IOrderPaginate } from "../../models/IOrderPaginate";
+import { IOrderProduct } from "../../models/IOrderProduct";
+import { IOrderRepositories, Pagination } from "../IOrderRepositories";
+
+export default class DummyOrderRepositories implements IOrderRepositories{
+  private orders: Order[] = [];
+
+  async create({customer, products}: ICreateOrder): Promise<Order>{
+  const order = new Order();
+
+  order.id = this.orders.length + 1;
+  order.customer = customer;
+  order.order_products = products as IOrderProduct[]
+
+  this.orders.push(order);
+
+  return order;
+  }
+
+  async save(order: Order): Promise<Order>{
+    const findIndex = this.orders.findIndex(findOrder => findOrder.id === order.id)
+
+    this.orders[findIndex] = order;
+
+    return order;
+  }
+
+  async findAll({page, skip, take}: SearchParams): Promise<IOrderPaginate> {
+    const orders = this.orders
+    const count = this.orders.length
+
+    const result = {
+      per_page: take,
+      total: count,
+      current_page: page,
+      data: orders,
+    };
+
+    return result as IOrderPaginate
+  }
+
+  async findById(id: number): Promise<Order | null> {
+    const order = this.orders.find(order => order.id === id)
+
+    return order as Order | null
+  }
+
+  findAndCount(pagination: Pagination): Promise<[Order[], number]> {
+    throw new AppError('Funcao nao implementada', 400)
+  }
+}
